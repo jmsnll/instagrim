5582426f0b1c6ff5842d170fe702b4010761611f
James Neill <jneill542@gmail.com>
2016-09-26 21:04:43 +0100

    Revert "Update table names"
    This reverts commit 2466df55afaf10d8ff3afadadb001f242efe07c7.

--- src/main/java/uk/ac/dundee/computing/tjn/instagrim/lib/Keyspaces.java
+++ src/main/java/uk/ac/dundee/computing/tjn/instagrim/lib/Keyspaces.java
@@ -11,8 +11,8 @@ public final class Keyspaces {
     public static void SetUpKeySpaces(Cluster c) {
         try {
             //Add some keyspaces here
-            String createKeyspace = "create keyspace if not exists instagrim  WITH replication = {'class':'SimpleStrategy', 'replication_factor':1}";
-            String createImagesTable = "CREATE TABLE if not exists instagrim.images ("
+            String createkeyspace = "create keyspace if not exists instagrim  WITH replication = {'class':'SimpleStrategy', 'replication_factor':1}";
+            String CreatePicTable = "CREATE TABLE if not exists instagrim.pics ("
                     + " user varchar,"
                     + " picid uuid, "
                     + " interaction_time timestamp,"
@@ -27,18 +27,18 @@ public final class Keyspaces {
                     + " name  varchar,"
                     + " PRIMARY KEY (picid)"
                     + ")";
-            String createUserImagesListTable = "CREATE TABLE if not exists instagrim.userImagesList (\n"
+            String Createuserpiclist = "CREATE TABLE if not exists instagrim.userpiclist (\n"
                     + "picid uuid,\n"
                     + "user varchar,\n"
                     + "pic_added timestamp,\n"
                     + "PRIMARY KEY (user,pic_added)\n"
                     + ") WITH CLUSTERING ORDER BY (pic_added desc);";
-            String createAddressTable = "CREATE TYPE if not exists instagrim.address (\n"
+            String CreateAddressType = "CREATE TYPE if not exists instagrim.address (\n"
                     + "      street text,\n"
                     + "      city text,\n"
                     + "      zip int\n"
                     + "  );";
-            String createAccountsTable = "CREATE TABLE if not exists instagrim.accounts (\n"
+            String CreateUserProfile = "CREATE TABLE if not exists instagrim.accounts (\n"
                     + "      username text PRIMARY KEY,\n"
                     + "      password text,\n"
                     + "      first_name text,\n"
@@ -49,7 +49,7 @@ public final class Keyspaces {
             Session session = c.connect();
             try {
                 PreparedStatement statement = session
-                        .prepare(createKeyspace);
+                        .prepare(createkeyspace);
                 BoundStatement boundStatement = new BoundStatement(
                         statement);
                 ResultSet rs = session
@@ -60,32 +60,32 @@ public final class Keyspaces {
             }
 
             //now add some column families 
-            System.out.println("" + createImagesTable);
+            System.out.println("" + CreatePicTable);
 
             try {
-                SimpleStatement cqlQuery = new SimpleStatement(createImagesTable);
+                SimpleStatement cqlQuery = new SimpleStatement(CreatePicTable);
                 session.execute(cqlQuery);
             } catch (Exception ex) {
                 System.out.println("Can't create tweet table " + ex);
             }
-            System.out.println("" + createUserImagesListTable);
+            System.out.println("" + Createuserpiclist);
 
             try {
-                SimpleStatement cqlQuery = new SimpleStatement(createUserImagesListTable);
+                SimpleStatement cqlQuery = new SimpleStatement(Createuserpiclist);
                 session.execute(cqlQuery);
             } catch (Exception ex) {
                 System.out.println("Can't create user pic list table " + ex);
             }
-            System.out.println("" + createAddressTable);
+            System.out.println("" + CreateAddressType);
             try {
-                SimpleStatement cqlQuery = new SimpleStatement(createAddressTable);
+                SimpleStatement cqlQuery = new SimpleStatement(CreateAddressType);
                 session.execute(cqlQuery);
             } catch (Exception ex) {
                 System.out.println("Can't create Address type " + ex);
             }
-            System.out.println("" + createAccountsTable);
+            System.out.println("" + CreateUserProfile);
             try {
-                SimpleStatement cqlQuery = new SimpleStatement(createAccountsTable);
+                SimpleStatement cqlQuery = new SimpleStatement(CreateUserProfile);
                 session.execute(cqlQuery);
             } catch (Exception ex) {
                 System.out.println("Can't create Address Profile " + ex);
@@ -95,5 +95,6 @@ public final class Keyspaces {
         } catch (Exception ex) {
             System.out.println("Other keyspace or coulm definition error" + ex);
         }
+
     }
 }
