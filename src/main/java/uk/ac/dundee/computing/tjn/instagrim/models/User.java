5f2d452ada3bf085682db32c80ec6d7343a9d06b
James Neill <jneill542@gmail.com>
2016-09-26 18:27:59 +0100

    Implemented password salting using PasswordStorage.java

--- src/main/java/uk/ac/dundee/computing/tjn/instagrim/models/User.java
+++ src/main/java/uk/ac/dundee/computing/tjn/instagrim/models/User.java
@@ -11,27 +11,27 @@ import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import uk.ac.dundee.computing.tjn.instagrim.lib.AeSimpleSHA1;
+import uk.ac.dundee.computing.tjn.instagrim.lib.PasswordStorage;
 import uk.ac.dundee.computing.tjn.instagrim.lib.TwoFactorAuthUtil;
 
 public class User {
 
     private String name;
     private String username;
-    private String passwordSHA1;
+    private String password;
     private String email;
     private Boolean emailVerificationStatus;
     private String base32secret;
 
-    private Cluster cluster;
+    private final Cluster cluster;
 
     TwoFactorAuthUtil twoFactorHandler = new TwoFactorAuthUtil();
 
     public User(String username, String password, Cluster cluster) {
         this.username = username;
         try {
-            this.passwordSHA1 = AeSimpleSHA1.SHA1(password);
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            this.password = PasswordStorage.createHash(password);
+        } catch (PasswordStorage.CannotPerformOperationException ex) {
             Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
         }
         this.cluster = cluster;
@@ -40,8 +40,8 @@ public class User {
     public User(String username, String password, String email, String name, Cluster cluster) {
         this.username = username;
         try {
-            this.passwordSHA1 = AeSimpleSHA1.SHA1(password);
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            this.password = PasswordStorage.createHash(password);
+        } catch (PasswordStorage.CannotPerformOperationException ex) {
             Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
         }
         this.email = email;
@@ -91,9 +91,9 @@ public class User {
 
     public boolean Register() {
         Session session = cluster.connect("instagrim");
-        PreparedStatement ps = session.prepare("insert into accounts (name, username, passwordSHA1, email) Values(?,?,?,?)");
+        PreparedStatement ps = session.prepare("insert into accounts (name, username, password, email) Values(?,?,?,?)");
         BoundStatement bs = new BoundStatement(ps);
-        session.execute(bs.bind(name, username, passwordSHA1, email));
+        session.execute(bs.bind(name, username, password, email));
         return true;
     }
 
@@ -106,30 +106,26 @@ public class User {
         return true;
     }
 
-    public boolean isValidUser(String username, String Password) {
-        String EncodedPassword = null;
-        try {
-            EncodedPassword = AeSimpleSHA1.SHA1(Password);
-        } catch (UnsupportedEncodingException | NoSuchAlgorithmException et) {
-            System.out.println("Can't check your password");
-            return false;
-        }
+    public boolean isValidUser(String username, String password) {
         Session session = cluster.connect("instagrim");
-        PreparedStatement ps = session.prepare("select passwordSHA1 from accounts where username = ?");
+        PreparedStatement ps = session.prepare("select password from accounts where username = ?");
         BoundStatement boundStatement = new BoundStatement(ps);
         ResultSet rs = session.execute(boundStatement.bind(username));
         if (rs.isExhausted()) {
-            System.out.println("No Images returned");
+            System.out.println("No accounts returned.");
             return false;
         } else {
             for (Row row : rs) {
-                String StoredPass = row.getString("passwordSHA1");
-                if (StoredPass.compareTo(EncodedPassword) == 0) {
-                    return true;
+                String storedPassword = row.getString("password");
+                try {
+                    if (PasswordStorage.verifyPassword(password, storedPassword)) {
+                        return true;
+                    }
+                } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
+                    Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         }
-
         return false;
     }
 
@@ -149,12 +145,16 @@ public class User {
         this.username = username;
     }
 
-    public String getPasswordSHA1() {
-        return passwordSHA1;
+    public String getPassword() {
+        return password;
     }
 
-    public void setPasswordSHA1(String passwordSHA1) {
-        this.passwordSHA1 = passwordSHA1;
+    public void setPassword(String password) {
+        try {
+            this.password = PasswordStorage.createHash(password);
+        } catch (PasswordStorage.CannotPerformOperationException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public String getEmail() {
