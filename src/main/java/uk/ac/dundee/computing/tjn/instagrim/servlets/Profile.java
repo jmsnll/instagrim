d8c03d90d5478b31db819b2c51d8d6ca1bdc11c9
James Neill <jneill542@gmail.com>
2016-09-30 14:29:12 +0100

    Grabs name of the profile & post requested then prints them out.

--- src/main/java/uk/ac/dundee/computing/tjn/instagrim/servlets/Profile.java
+++ src/main/java/uk/ac/dundee/computing/tjn/instagrim/servlets/Profile.java
@@ -1,44 +1,48 @@
 package uk.ac.dundee.computing.tjn.instagrim.servlets;
 
+import com.datastax.driver.core.Cluster;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import uk.ac.dundee.computing.tjn.instagrim.lib.CassandraHosts;
+import uk.ac.dundee.computing.tjn.instagrim.lib.Convertors;
 
-@WebServlet(name = "Profile", urlPatterns = {"/Profile/*"}, initParams = {
+@WebServlet(name = "Profile", urlPatterns = {"/u/*"}, initParams = {
     @WebInitParam(name = "Name", value = "Value")})
 public class Profile extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Profile</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Profile at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+    private Cluster cluster;
+    
+    public Profile() {
+        
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        this.cluster = CassandraHosts.getCluster();
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String args[] = Convertors.SplitRequestPath(request);
+        String profileName = args[2];
+        String postID = args[3];
+        try (PrintWriter pw = response.getWriter()) {
+            pw.write("Profile: " + profileName);
+            pw.write("\nPost ID: " + postID);
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+
     }
 }
