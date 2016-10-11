de8fd5b8e7ddb02f103967f4546bba853886a227
James Neill <jneill542@gmail.com>
2016-10-11 01:09:02 +0100

    Added postcomments table and changed some fields

--- src/main/java/uk/ac/dundee/computing/tjn/instagrim/lib/Keyspaces.java
+++ src/main/java/uk/ac/dundee/computing/tjn/instagrim/lib/Keyspaces.java
@@ -27,13 +27,20 @@ public final class Keyspaces {
                     + " name  varchar,"
                     + " PRIMARY KEY (imageid)"
                     + ")";
+            String createCommentsTable = "CREATE TABLE if not exists instagrim.postcomments ("
+                    + "commentid uuid,"
+                    + "user varchar,"
+                    + "comment_posted timestamp,"
+                    + "caption text,"
+                    + "PRIMARY KEY (commentid)"
+                    + ")";
             String createPostsTable = "CREATE TABLE if not exists instagrim.posts (\n"
                     + "imageid uuid,\n"
                     + "user varchar,\n"
                     + "image_added timestamp,\n"
                     + "caption text,\n"
                     + "likes set<text>,\n"
-                    + "comments set<text>,\n"
+                    + "comments set<uuid>,\n"
                     + "PRIMARY KEY (user,image_added)\n"
                     + ") WITH CLUSTERING ORDER BY (image_added desc);";
             String createAccountsTable = "CREATE TABLE if not exists instagrim.accounts (\n"
@@ -45,7 +52,7 @@ public final class Keyspaces {
                     + "      emailVerified boolean,\n"
                     + "      base32secret text,\n"
                     + "      bio text,\n"
-                    + "      profile_pic blob\n"
+                    + "      profile_pic uuid\n"
                     + "  );";
             Session session = c.connect();
             try {
@@ -74,7 +81,16 @@ public final class Keyspaces {
             } catch (Exception ex) {
                 System.out.println("Can't create user image list table " + ex);
             }
+            System.out.println("" + createCommentsTable);
+
+            try {
+                SimpleStatement cqlQuery = new SimpleStatement(createCommentsTable);
+                session.execute(cqlQuery);
+            } catch (Exception ex) {
+                System.out.println("Can't create post comments table " + ex);
+            }
             System.out.println("" + createAccountsTable);
+
             try {
                 SimpleStatement cqlQuery = new SimpleStatement(createAccountsTable);
                 session.execute(cqlQuery);
