3cb08fe7e12304b32071e67f7b71ce391adce579
James Neill <jneill542@gmail.com>
2016-10-23 15:06:37 +0100

    Added Search servlet

---
+++ src/main/java/uk/ac/dundee/computing/tjn/instagrim/servlets/Search.java
@@ -0,0 +1,20 @@
+package uk.ac.dundee.computing.tjn.instagrim.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Search", urlPatterns = {"/search"})
+public class Search extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
